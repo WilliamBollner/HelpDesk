b07cc060b19bfb6726c9fce5342921714831068b
WilliamBollner <williambollner@hotmail.com>
2022-06-03 10:23:33 -0300

    Projeto iniciado

---
+++ src/test/java/br/com/williamdev/helpdesk/HelpDeskApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.williamdev.helpdesk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpDeskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
