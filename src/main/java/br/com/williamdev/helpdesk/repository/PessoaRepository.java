0f0d56873fc94739a5fb634523014fbeff32aae3
William Bollner <williambollner@hotmail.com>
2022-08-30 19:02:18 -0300

    exceção para cpf e email já cadastrado

--- src/main/java/br/com/williamdev/helpdesk/repository/PessoaRepository.java
+++ src/main/java/br/com/williamdev/helpdesk/repository/PessoaRepository.java
@@ -1,11 +1,14 @@
 package br.com.williamdev.helpdesk.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.williamdev.helpdesk.domain.Pessoa;
 
 public interface PessoaRepository extends JpaRepository<Pessoa, Integer>{
 
-	
+	Optional<Pessoa> findByCpf(String cpf);
+	Optional<Pessoa> findByEmail(String email);
 
 }
