1d6f41163ee84004baa2cade6cbc8497dd1db145
William Bollner <williambollner@hotmail.com>
2022-08-29 22:03:30 -0300

    endpoint cerate para tecnico

--- src/main/java/br/com/williamdev/helpdesk/resources/TecnicoResource.java
+++ src/main/java/br/com/williamdev/helpdesk/resources/TecnicoResource.java
@@ -1,15 +1,23 @@
 package br.com.williamdev.helpdesk.resources;
 
+import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.williamdev.helpdesk.domain.Tecnico;
+import br.com.williamdev.helpdesk.domain.dtos.TecnicoDTO;
 import br.com.williamdev.helpdesk.service.TecnicoService;
-
+ 
 @RestController
 @RequestMapping("/tecnicos")
 public class TecnicoResource {
@@ -18,9 +26,23 @@ public class TecnicoResource {
 	private TecnicoService service;
 	
 	@GetMapping("/{id}")
-	public ResponseEntity<Tecnico> findById(@PathVariable Integer id){
+	public ResponseEntity<TecnicoDTO> findById(@PathVariable Integer id){
 		Tecnico obj = service.findById(id);
-		return ResponseEntity.ok().body(obj);
+		return ResponseEntity.ok().body(new TecnicoDTO(obj));
+	}
+	
+	@GetMapping
+	public ResponseEntity<List<TecnicoDTO>> findAll(){
+		List<Tecnico> list = service.findAll();
+		List<TecnicoDTO> listDTO = list.stream().map(obj -> new TecnicoDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+	}
+	
+	@PostMapping
+	public ResponseEntity<TecnicoDTO> create(@RequestBody TecnicoDTO objDTO){
+		Tecnico newObj = service.create(objDTO);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newObj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
 	}
 	
 
--- src/main/java/br/com/williamdev/helpdesk/service/TecnicoService.java
+++ src/main/java/br/com/williamdev/helpdesk/service/TecnicoService.java
@@ -1,12 +1,15 @@
 package br.com.williamdev.helpdesk.service;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.com.williamdev.helpdesk.domain.Tecnico;
+import br.com.williamdev.helpdesk.domain.dtos.TecnicoDTO;
 import br.com.williamdev.helpdesk.repository.TecnicoRepository;
+import br.com.williamdev.helpdesk.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class TecnicoService {
@@ -16,7 +19,17 @@ public class TecnicoService {
 
 	public Tecnico findById(Integer id) {
 		Optional<Tecnico> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found. Id: " + id));
+	}
+	
+	public List<Tecnico> findAll(){
+		return repository.findAll();
+	}
+
+	public Tecnico create(TecnicoDTO objDTO) {
+		objDTO.setId(null);
+		Tecnico newObj = new Tecnico(objDTO);
+		return repository.save(newObj);
 	}
 	
 }
