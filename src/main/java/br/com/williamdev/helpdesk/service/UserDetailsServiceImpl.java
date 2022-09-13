dfcd1feb541d2c948611bec6cbabab63129cddca
William Bollner <williambollner@hotmail.com>
2022-09-13 19:59:56 -0300

    Autenticacao com JWT

---
+++ src/main/java/br/com/williamdev/helpdesk/service/UserDetailsServiceImpl.java
@@ -0,0 +1,30 @@
+package br.com.williamdev.helpdesk.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import br.com.williamdev.helpdesk.domain.Pessoa;
+import br.com.williamdev.helpdesk.repository.PessoaRepository;
+import br.com.williamdev.helpdesk.security.UserSS;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService{
+	
+	@Autowired
+	private PessoaRepository repository;
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		Optional<Pessoa> user = repository.findByEmail(email);
+		if(user.isPresent()) {
+			return new UserSS(user.get().getId(), user.get().getEmail(), user.get().getSenha(), user.get().getPerfis());
+		}
+		throw new UsernameNotFoundException(email);
+	}
+
+}
